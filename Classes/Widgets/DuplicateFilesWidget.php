c346eca2aa351293502dd9c7376f3a48dc39e87a
Benjamin Tammling <benjamin.tammling@sitegeist.de>
2023-06-07 12:27:17 +0200

    Add widget for duplicate files

---
+++ Classes/Widgets/DuplicateFilesWidget.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Sitegeist\WidgetMirror\Widgets;
+
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Resource\ResourceFactory;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Dashboard\Widgets\WidgetConfigurationInterface;
+use TYPO3\CMS\Dashboard\Widgets\WidgetInterface;
+use TYPO3\CMS\Fluid\View\StandaloneView;
+
+class DuplicateFilesWidget implements WidgetInterface
+{
+    /**
+     * @var WidgetConfigurationInterface
+     */
+    private $configuration;
+
+    /**
+     * @var StandaloneView
+     */
+    private $view;
+
+    /**
+     * @var ConnectionPool
+     */
+    private $connectionPool;
+
+    public function __construct(
+        WidgetConfigurationInterface $configuration,
+        StandaloneView               $view,
+        ConnectionPool               $connectionPool
+    )
+    {
+        $this->configuration = $configuration;
+        $this->view = $view;
+        $this->connectionPool = $connectionPool;
+    }
+
+    public function renderWidgetContent(): string
+    {
+        $resourceFactory = GeneralUtility::makeInstance(ResourceFactory::class);
+        $queryBuilder = $this->connectionPool->getConnectionForTable('sys_file')->createQueryBuilder();
+        $queryBuilder->getRestrictions()->removeAll();
+
+        $duplicates = $queryBuilder
+            ->selectLiteral('GROUP_CONCAT(uid) as uids', 'sha1', 'count(*) as counting')
+            ->from('sys_file')
+            ->addOrderBy('counting', 'desc')
+            ->where('name != "index.html"')
+            ->groupBy('sha1')
+            ->having('counting > 1')
+            ->execute()
+            ->fetchAllAssociative();
+
+        foreach ($duplicates as &$duplicate) {
+            $duplicate['files'] = array_map(
+                function ($uid) use ($resourceFactory) {
+                    return $resourceFactory->getFileObject($uid);
+                },
+                GeneralUtility::trimExplode(',', $duplicate['uids'])
+            );
+
+        }
+
+        $this->view->setTemplate('Widget/DuplicateFilesWidget');
+        $this->view->assignMultiple([
+            'duplicates' => $duplicates,
+            'configuration' => $this->configuration
+        ]);
+
+        return $this->view->render();
+    }
+}
