bc6d7a5f6bdd651540272e72f7c8d6fd0ade7508
Benjamin Tammling <benjamin.tammling@sitegeist.de>
2023-06-08 14:54:42 +0200

    Add widget for showing the last changed pages

---
+++ Classes/Widgets/LastChangedPagesWidget.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Sitegeist\WidgetMirror\Widgets;
+
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Resource\ResourceFactory;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\RootlineUtility;
+use TYPO3\CMS\Dashboard\Widgets\WidgetConfigurationInterface;
+use TYPO3\CMS\Dashboard\Widgets\WidgetInterface;
+use TYPO3\CMS\Fluid\View\StandaloneView;
+
+class LastChangedPagesWidget implements WidgetInterface
+{
+    /**
+     * @var WidgetConfigurationInterface
+     */
+    private $configuration;
+
+    /**
+     * @var StandaloneView
+     */
+    private $view;
+
+    /**
+     * @var ConnectionPool
+     */
+    private $connectionPool;
+
+    public function __construct(
+        WidgetConfigurationInterface $configuration,
+        StandaloneView               $view,
+        ConnectionPool               $connectionPool
+    )
+    {
+        $this->configuration = $configuration;
+        $this->view = $view;
+        $this->connectionPool = $connectionPool;
+    }
+
+    public function renderWidgetContent(): string
+    {
+        $queryBuilder = $this->connectionPool->getConnectionForTable('sys_file')->createQueryBuilder();
+
+        $pages = $queryBuilder
+            ->select('uid', 'tstamp')
+            ->from('pages')
+            ->addOrderBy('tstamp', 'desc')
+            ->where($GLOBALS['BE_USER']->getPagePermsClause(1))
+            ->setMaxResults(10)
+            ->execute()
+            ->fetchAllAssociative();
+
+        foreach ($pages as &$page) {
+            $rootlineUtility = GeneralUtility::makeInstance(RootlineUtility::class, $page['uid']);
+            $page['rootline'] = implode(
+                ' -> ',
+                array_slice(
+                    array_map(function ($page) {
+                            return $page['title'];
+                        }, array_reverse($rootlineUtility->get())
+                    ),
+                1)
+            );
+        }
+        $this->view->setTemplate('Widget/LastChangedPagesWidget');
+        $this->view->assignMultiple([
+            'pages' => $pages,
+            'configuration' => $this->configuration
+        ]);
+
+        return $this->view->render();
+    }
+}
