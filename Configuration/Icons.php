9d68be5a2119b3debd870f6a3d570ffadcddb152
Ulrich Mathes <mathes@sitegeist.de>
2023-09-07 08:02:17 +0200

    use designed icon for all widgets

---
+++ Configuration/Icons.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
+return [
+    'widget-mirror' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:widget_mirror/Resources/Public/Icons/Extension.svg',
+    ],
+];
