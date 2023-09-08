d1bb27646fb53e41ffcfc1f544fdd77ac669d522
Ulrich Mathes <mathes@sitegeist.de>
2023-09-08 21:34:21 +0200

    use GeneralUtility fo calculate bytes from string

--- Classes/Widgets/StorageSizeWidget.php
+++ Classes/Widgets/StorageSizeWidget.php
@@ -93,7 +93,9 @@ class StorageSizeWidget implements WidgetInterface, EventDataInterface, Addition
         ];
 
         if (!empty($storage->getStorageRecord()['tx_widget_mirror_max_size'])) {
-            $maxSize = $this->humanFileSizeToBytes($storage->getStorageRecord()['tx_widget_mirror_max_size']);
+            $maxSize = GeneralUtility::getBytesFromSizeMeasurement(
+                $storage->getStorageRecord()['tx_widget_mirror_max_size']
+            );
         } else {
             $maxSize = self::DEFAULT_MAX_SIZE;
         }
@@ -108,30 +110,11 @@ class StorageSizeWidget implements WidgetInterface, EventDataInterface, Addition
         return $data;
     }
 
-    protected function getDirSize($path)
+    protected function getDirSize($path): int
     {
         $fio = popen('/usr/bin/du -sb '.$path, 'r');
         $size = intval(fgets($fio,80));
         pclose($fio);
         return $size;
     }
-
-    protected function humanFileSizeToBytes($size)
-    {
-        $size = trim($size);
-        $units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-        preg_match('/^(\d+(\.\d+)?)\s*([A-Za-z]+)?$/', $size, $matches);
-        if (empty($matches)) {
-            throw new \InvalidArgumentException('Invalid file size format');
-        }
-        $value = (float) $matches[1];
-        $unit = strtoupper($matches[3] ?? 'B');
-
-        if (!in_array($unit, $units)) {
-            throw new \InvalidArgumentException('Invalid file size unit');
-        }
-        $exponent = array_search($unit, $units);
-        return round($value * (1024 ** $exponent));
-    }
-
 }
