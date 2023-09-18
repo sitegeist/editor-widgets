e3b9f9f0565d1a0ba4770020728a7ecdacb52a2d
Benjamin Tammling <benjamin.tammling@sitegeist.de>
2023-09-18 13:38:37 +0200

    Add message when no broken links are found or linkvalidator is not installes

--- Classes/Widgets/BrokenLinksWidget.php
+++ Classes/Widgets/BrokenLinksWidget.php
@@ -4,6 +4,7 @@ namespace Sitegeist\WidgetMirror\Widgets;
 
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Localization\LanguageServiceFactory;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Dashboard\Widgets\WidgetConfigurationInterface;
 use TYPO3\CMS\Dashboard\Widgets\WidgetInterface;
@@ -22,29 +23,30 @@ class BrokenLinksWidget implements WidgetInterface
 
     public function renderWidgetContent(): string
     {
-        $queryBuilder = $this->connectionPool->getConnectionForTable('tx_linkvalidator_link')->createQueryBuilder();
         $languageServiceFactory = GeneralUtility::makeInstance(LanguageServiceFactory::class);
         $GLOBALS['LANG'] = $languageServiceFactory->createFromUserPreferences($GLOBALS['BE_USER']);
         $GLOBALS['LANG']->includeLLFile('EXT:linkvalidator/Resources/Private/Language/Module/locallang.xlf');
 
-        $brokenLinks = $queryBuilder
-            ->select('*')
-            ->from('tx_linkvalidator_link')
-            ->addOrderBy('last_check', 'desc')
-            ->execute()
-            ->fetchAllAssociative();
+        if (ExtensionManagementUtility::isLoaded('linkvalidator')) {
+            $queryBuilder = $this->connectionPool->getConnectionForTable('tx_linkvalidator_link')->createQueryBuilder();
 
-        foreach ($brokenLinks as &$brokenLink) {
-            /** @var AbstractLinktype $linkType */
-            $linkType = GeneralUtility::makeInstance($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['linkvalidator']['checkLinks'][$brokenLink['link_type']]);
-            $brokenLink['errorMessage'] = $linkType->getErrorMessage(json_decode($brokenLink['url_response'], true)['errorParams']);
+            $brokenLinks = $queryBuilder
+                ->select('*')
+                ->from('tx_linkvalidator_link')
+                ->addOrderBy('last_check', 'desc')
+                ->execute()
+                ->fetchAllAssociative();
+
+            foreach ($brokenLinks as &$brokenLink) {
+                /** @var AbstractLinktype $linkType */
+                $linkType = GeneralUtility::makeInstance($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['linkvalidator']['checkLinks'][$brokenLink['link_type']]);
+                $brokenLink['errorMessage'] = $linkType->getErrorMessage(json_decode($brokenLink['url_response'], true)['errorParams']);
+            }
+
+            $this->view->assign('brokenLinks', $brokenLinks);
         }
-
+        $this->view->assign('configuration', $this->configuration);
         $this->view->setTemplate('Widget/BrokenLinksWidget');
-        $this->view->assignMultiple([
-            'brokenLinks' => $brokenLinks,
-            'configuration' => $this->configuration
-        ]);
 
         return $this->view->render();
     }
