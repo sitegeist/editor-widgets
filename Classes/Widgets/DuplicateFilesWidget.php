6c32e8de872db5fcec42031c40297e373ab66773
Ulrich Mathes <mathes@sitegeist.de>
2023-10-12 22:01:56 +0200

    DuplicateFiles: get parentfolder in fluid

--- Classes/Widgets/DuplicateFilesWidget.php
+++ Classes/Widgets/DuplicateFilesWidget.php
@@ -4,7 +4,6 @@ namespace Sitegeist\WidgetMirror\Widgets;
 
 use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Database\ConnectionPool;
-use TYPO3\CMS\Core\Resource\Exception\FolderDoesNotExistException;
 use TYPO3\CMS\Core\Resource\ResourceFactory;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Dashboard\Widgets\WidgetConfigurationInterface;
@@ -14,16 +13,16 @@ use TYPO3\CMS\Fluid\View\StandaloneView;
 class DuplicateFilesWidget implements WidgetInterface
 {
     public function __construct(
-        private ?ConnectionPool $connectionPool = null,
-        private ?StandaloneView $view = null,
-        private ?WidgetConfigurationInterface $configuration = null,
+        private ConnectionPool $connectionPool,
+        private ResourceFactory $resourceFactory,
+        private StandaloneView $view,
+        private WidgetConfigurationInterface $configuration,
         private readonly array $options = []
     )
     {}
 
     public function renderWidgetContent(): string
     {
-        $resourceFactory = GeneralUtility::makeInstance(ResourceFactory::class);
         $queryBuilder = $this->connectionPool->getConnectionForTable('sys_file')->createQueryBuilder();
         $queryBuilder->getRestrictions()->removeAll();
 
@@ -39,23 +38,16 @@ class DuplicateFilesWidget implements WidgetInterface
 
         foreach ($duplicates as &$duplicate) {
             $duplicate['files'] = array_map(
-                function ($uid) use ($resourceFactory) {
-                    $file = $resourceFactory->getFileObject($uid);
-                    try {
-                        $parentFolder = $file->getParentFolder()->getCombinedIdentifier();
-                    } catch (FolderDoesNotExistException $e) {
-                        $parentFolder = '???';
-                    }
+                function ($uid) {
+                    $file = $this->resourceFactory->getFileObject($uid);
                     return [
                         'file' => $file,
                         'referenceCount' => BackendUtility::referenceCount('sys_file', $file->getUid()),
-                        'parentFolder' => $parentFolder,
                         'isImage' => $file->isImage(),
                     ];
                 },
                 GeneralUtility::trimExplode(',', $duplicate['uids'])
             );
-
         }
 
         $this->view->setTemplate('Widget/DuplicateFilesWidget');
