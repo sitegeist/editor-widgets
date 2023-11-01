e136bc3279496e93060494c924e313742954896b
Ulrich Mathes <mathes@sitegeist.de>
2023-11-01 14:59:56 +0100

    show icon and text if latest redirects list is empty

---
+++ ext_tables.php
@@ -0,0 +1,6 @@
+<?php
+defined('TYPO3') or die();
+
+(static function () {
+    $GLOBALS['TBE_STYLES']['skins']['dashboard']['stylesheetDirectories']['widget_mirror'] = 'EXT:widget_mirror/Resources/Public/Css/';
+})();
