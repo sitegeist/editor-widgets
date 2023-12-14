9cd9e21607b740416d0873d9074a23e4299ca6c4
Ulrich Mathes <mathes@sitegeist.de>
2023-12-14 16:17:24 +0100

    [FEATURE] hide broken links by editors choice

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,4 +1,4 @@
 <?php
-defined('TYPO3_MODE') || die('Access denied.');
+defined('TYPO3') || die('Access denied.');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('editor_widgets', 'Configuration/TypoScript', 'Editor Widgets');
