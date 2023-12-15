56954aa881b14ce3d238da592eac33f241eb0f5b
Ulrich Mathes <mathes@sitegeist.de>
2023-12-15 12:17:12 +0100

    [TASK] force include of typoscript for widget templateRootPath

--- Configuration/TCA/Overrides/sys_template.php
+++
@@ -1,4 +0,0 @@
-<?php
-defined('TYPO3') || die('Access denied.');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('editor_widgets', 'Configuration/TypoScript', 'Editor Widgets');
--- ext_localconf.php
+++ ext_localconf.php
@@ -4,8 +4,15 @@ declare(strict_types=1);
 
 use TYPO3\CMS\Core\Cache\Backend\FileBackend;
 use TYPO3\CMS\Core\Cache\Frontend\VariableFrontend;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3') or die();
+
+ExtensionManagementUtility::addTypoScript(
+    'editor_widgets',
+    'setup',
+    "@import 'EXT:editor_widgets/Configuration/TypoScript/setup.typoscript'"
+);
 
 if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['editor_widgets_storage_size'] ?? null)) {
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['editor_widgets_storage_size'] = [
