d34d0105b852b02cca41e942305725c0ea85a1eb
Ulrich Mathes <mathes@sitegeist.de>
2024-02-01 08:40:49 +0100

    BrokenLinks: do not extend linkvalidator table

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,10 +2,3 @@ CREATE TABLE sys_file_storage
 (
 	tx_editor_widgets_max_size VARCHAR(255) NOT NULL DEFAULT '1GB'
 );
-
--- pid is necessary for DataHandler
-CREATE TABLE tx_linkvalidator_link
-(
-	tx_editor_widgets_hidden tinyint(1) unsigned DEFAULT '0' NOT NULL,
-	pid tinyint(1) unsigned DEFAULT '0' NOT NULL,
-);
