f9631c2d86cadef7943baa9d490a4c659e41eb50
Ulrich Mathes <mathes@sitegeist.de>
2024-02-01 09:03:40 +0100

    BrokenLinks: add definition for persisted linkvalidator links

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,3 +2,9 @@ CREATE TABLE sys_file_storage
 (
 	tx_editor_widgets_max_size VARCHAR(255) NOT NULL DEFAULT '1GB'
 );
+
+CREATE TABLE tx_editor_widgets_broken_link
+(
+	linkvalidator_link VARCHAR(255) DEFAULT '' NOT NULL,
+	suppressed SMALLINT UNSIGNED DEFAULT '0' NOT NULL,
+);
