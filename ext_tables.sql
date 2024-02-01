51944f75e29f27ea79defd1be628d0818a7c231b
Ulrich Mathes <mathes@sitegeist.de>
2024-02-01 11:06:42 +0100

    BrokenLinks: mark link hash column as unique

--- ext_tables.sql
+++ ext_tables.sql
@@ -5,6 +5,6 @@ CREATE TABLE sys_file_storage
 
 CREATE TABLE tx_editor_widgets_broken_link
 (
-	linkvalidator_link VARCHAR(255) DEFAULT '' NOT NULL,
+	linkvalidator_link VARCHAR(255) DEFAULT '' NOT NULL UNIQUE,
 	suppressed SMALLINT UNSIGNED DEFAULT '0' NOT NULL,
 );
