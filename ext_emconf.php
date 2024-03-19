d02f94db0b3b23bb2a26680fbcc9295753e1ca5e
Ulrich Mathes <mathes@sitegeist.de>
2024-03-19 15:08:43 +0100

    [TASK] set version in ext_emconf from current tag

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,29 +1,28 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Editor Widgets',
-    'description' => 'Collection of useful dashboard widgets focused on editors',
-    'category' => 'backend',
-    'author' => 'Benjamin Tammling',
-    'author_email' => 'benjamin.tammling@sitegeist.de',
-    'author_company' => 'sitegeist media solutions GmbH',
-    'state' => 'beta',
-    'uploadfolder' => false,
-    'clearCacheOnLoad' => true,
-    'version' => '1.2.2',
-    'constraints' => [
-        'depends' => [
-            'typo3' => '11.5.0-11.5.99',
-            'php' => '8.1.0-8.2.99'
+    "title" => "Editor Widgets",
+    "description" => "Collection of useful dashboard widgets focused on editors",
+    "category" => "backend",
+    "author" => "Benjamin Tammling, Ulrich Mathes",
+    "author_email" => "benjamin.tammling@sitegeist.de, mathes@sitegeist.de",
+    "author_company" => "sitegeist media solutions GmbH",
+    "state" => "stable",
+    "uploadfolder" => false,
+    "clearCacheOnLoad" => true,
+    "constraints" => [
+        "depends" => [
+            "typo3" => "11.5.0-11.5.99",
+            "php" => "8.1.0-8.2.99"
         ],
-        'conflicts' => [
+        "conflicts" => [
         ],
-        'suggests' => [
-            'typo3/cms-linkvalidator' => '*',
+        "suggests" => [
+            "typo3/cms-linkvalidator" => "*",
         ],
     ],
-    'autoload' => [
-        'psr-4' => [
-            'Sitegeist\\EditorWidgets\\' => 'Classes'
+    "autoload" => [
+        "psr-4" => [
+            "Sitegeist\\EditorWidgets\\" => "Classes"
         ]
     ],
 ];
