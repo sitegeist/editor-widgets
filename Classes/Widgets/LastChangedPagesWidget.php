1a28e516f67ee364570093e68fe3869d053bc311
Ulrich Mathes <mathes@sitegeist.de>
2024-04-09 12:56:52 +0200

    [BUGFIX] use sys_history as source for lastChangedPages
    as pages tstamp does not update on tt_content changes

--- Classes/Widgets/LastChangedPagesWidget.php
+++ Classes/Widgets/LastChangedPagesWidget.php
@@ -2,12 +2,11 @@
 
 namespace Sitegeist\EditorWidgets\Widgets;
 
-use TYPO3\CMS\Backend\History\RecordHistory;
 use TYPO3\CMS\Backend\Routing\PreviewUriBuilder;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Context\Context;
 use TYPO3\CMS\Core\Database\ConnectionPool;
-use TYPO3\CMS\Core\Database\Query\Restriction\HiddenRestriction;
+use TYPO3\CMS\Core\Database\Query\Restriction\DeletedRestriction;
 use TYPO3\CMS\Core\Database\Query\Restriction\WorkspaceRestriction;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\RootlineUtility;
@@ -16,8 +15,15 @@ use TYPO3\CMS\Dashboard\Widgets\WidgetConfigurationInterface;
 use TYPO3\CMS\Dashboard\Widgets\WidgetInterface;
 use TYPO3\CMS\Fluid\View\StandaloneView;
 
+/**
+ * Fetches many sys_history entries and finds it's related page uids.
+ * The latest pages are fetched respecting backend user access.
+ */
 class LastChangedPagesWidget implements WidgetInterface, AdditionalCssInterface
 {
+    const NUM_ENTRIES = 10;
+    const NUM_FETCH_SYS_HISTORY_ENTRIES = 1000;
+
     public function __construct(
         private ConnectionPool $connectionPool,
         private StandaloneView $view,
@@ -31,27 +37,77 @@ class LastChangedPagesWidget implements WidgetInterface, AdditionalCssInterface
 
     public function renderWidgetContent(): string
     {
-        $queryBuilder = $this->connectionPool->getConnectionForTable('pages')->createQueryBuilder();
-        $queryBuilder->getRestrictions()
-            ->removeByType(HiddenRestriction::class)
-            ->add(
-                GeneralUtility::makeInstance(
-                    WorkspaceRestriction::class,
-                    GeneralUtility::makeInstance(Context::class)->getPropertyFromAspect('workspace', 'id')
-                )
-            );
+        $workspaceRestriction = GeneralUtility::makeInstance(
+            WorkspaceRestriction::class,
+            GeneralUtility::makeInstance(Context::class)->getPropertyFromAspect('workspace', 'id')
+        );
 
-        $pages = $queryBuilder
-            ->select('*')
-            ->from('pages')
+        $queryBuilder = $this->connectionPool->getConnectionForTable('sys_history')->createQueryBuilder();
+        $queryBuilder->getRestrictions()->add($workspaceRestriction);
+
+        $queryBuilderTtContent = $this->connectionPool->getConnectionForTable('tt_content')->createQueryBuilder();
+        $queryBuilderTtContent->getRestrictions()->removeAll();
+
+        $queryBuilderPages = $this->connectionPool->getConnectionForTable('pages')->createQueryBuilder();
+        $queryBuilderPages->getRestrictions()
+            ->removeAll()
+            ->add(GeneralUtility::makeInstance(DeletedRestriction::class))
+            ->add($workspaceRestriction);
+
+        $history = $queryBuilder
+            ->select('tablename', 'recuid', 'tstamp', 'userid')
+            ->from('sys_history')
+            ->where($queryBuilder->expr()->in('tablename', [
+                $queryBuilder->createNamedParameter('pages'),
+                $queryBuilder->createNamedParameter('tt_content'),
+            ]))
             ->addOrderBy('tstamp', 'desc')
-            ->where($GLOBALS['BE_USER']->getPagePermsClause(1))
-            ->setMaxResults(10)
+            ->setMaxResults(self::NUM_FETCH_SYS_HISTORY_ENTRIES)
             ->execute()
             ->fetchAllAssociative();
 
-        foreach ($pages as &$page) {
-            $rootlineUtility = GeneralUtility::makeInstance(RootlineUtility::class, $page['uid']);
+
+        $latestPages = [];
+
+        foreach ($history as $historyEntry) {
+            if ($historyEntry['tablename'] == 'tt_content') {
+                $pid = $queryBuilderTtContent
+                    ->select('pid')
+                    ->from('tt_content')
+                    ->where($queryBuilderTtContent->expr()->eq('uid', $queryBuilderTtContent->createNamedParameter($historyEntry['recuid'])))
+                    ->execute()
+                    ->fetchOne();
+            } else {
+                $pid = $historyEntry['recuid'];
+            }
+
+            if (isset($latestPages[$pid])) {
+                continue;
+            }
+
+            // deleted pages don't show up in list
+            $page = $queryBuilderPages
+                ->select('*')
+                ->from('pages')
+                ->where($queryBuilderPages->expr()->eq('uid', $queryBuilderPages->createNamedParameter($pid)))
+                ->andWhere($GLOBALS['BE_USER']->getPagePermsClause(1))
+                ->execute()
+                ->fetchAssociative();
+
+            if (!$page) {
+                continue;
+            }
+
+            $latestPages[$pid] = $page;
+            $latestPages[$pid]['history'] = $historyEntry;
+
+            if (count($latestPages) == self::NUM_ENTRIES) {
+                break;
+            }
+        }
+
+        foreach ($latestPages as $pageId => &$page) {
+            $rootlineUtility = GeneralUtility::makeInstance(RootlineUtility::class, $pageId);
             $page['rootline'] = implode(
                 ' / ',
                 array_slice(
@@ -63,15 +119,16 @@ class LastChangedPagesWidget implements WidgetInterface, AdditionalCssInterface
                 -1)
             );
 
-            $page['viewLink'] = (string)PreviewUriBuilder::create($page['uid'])
-                ->withRootLine(BackendUtility::BEgetRootLine($page['uid']))
+            $page['viewLink'] = (string)PreviewUriBuilder::create($pageId)
+                ->withRootLine(BackendUtility::BEgetRootLine($pageId))
                 ->buildUri();
 
-            $page['userName'] = $this->getUserNameOfLatestChange($page['uid']);
+            $page['history']['userName'] = $this->userNames[$page['history']['userid']]['username'] ?? '';
         }
+
         $this->view->setTemplate('Widget/LastChangedPagesWidget');
         $this->view->assignMultiple([
-            'pages' => $pages,
+            'pages' => $latestPages,
             'configuration' => $this->configuration,
             'dateFormat' => $GLOBALS['TYPO3_CONF_VARS']['SYS']['ddmmyy'] . ' ' . $GLOBALS['TYPO3_CONF_VARS']['SYS']['hhmm']
         ]);
@@ -89,18 +146,5 @@ class LastChangedPagesWidget implements WidgetInterface, AdditionalCssInterface
        return [
            'EXT:editor_widgets/Resources/Public/Css/backend.css',
        ];
-    }
-
-    private function getUserNameOfLatestChange(int $pageUid): string
-    {
-        $history = GeneralUtility::makeInstance(RecordHistory::class, 'pages:' . $pageUid);
-        $history->setMaxSteps(1);
-        $latestChange = array_shift($history->getChangeLog());
-
-        if (!isset($latestChange['userid'])) {
-            return '';
-        }
-
-        return $this->userNames[$latestChange['userid']]['username'] ?? '';
     }
 }
